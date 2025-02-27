7bdde53f79b937a7fe23f6c8e3ceacd47dbaeaf0
OnikArt <onik.artusan@gmail.com>
2025-02-27 17:45:15 +0300

    Добавлена возможность изменение скорости каждой планеты, исправлено движение планет и свет

--- src/init.js
+++ src/init.js
@@ -1,25 +1,36 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const init = () => {
 	const sizes = {
 		width: window.innerWidth,
 		height: window.innerHeight,
-	};
+	}
 
-	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-	const canvas = document.querySelector('.canvas');
-	scene.add(camera);
+	const scene = new THREE.Scene()
+	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+	const canvas = document.querySelector('.canvas')
+	scene.add(camera)
 
-	const controls = new OrbitControls(camera, canvas);
-	controls.enableDamping = true;
-	
-	const renderer = new THREE.WebGLRenderer( {canvas} );
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.render(scene, camera);
+	// const hemisphereLight = new THREE.HemisphereLight(0xaaccff, 0x001122, 0.3)
+	// scene.add(hemisphereLight)
 
-	return {sizes, scene, camera, canvas, controls, renderer}
+	const sunLight = new THREE.DirectionalLight(0xffffff, 1.5);
+	sunLight.castShadow = true;
+	sunLight.position.x = -10;
+	const helper = new THREE.DirectionalLightHelper( sunLight, 5 );
+	scene.add( helper );
+	scene.add(sunLight)
+
+	const controls = new OrbitControls(camera, canvas)
+	controls.enableDamping = true
+
+	const renderer = new THREE.WebGLRenderer({ canvas })
+	renderer.shadowMap.enabled = true;
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.render(scene, camera)
+
+	return { sizes, scene, camera, canvas, controls, renderer, sunLight}
 }
 
 export default init
--- src/script.js
+++ src/script.js
@@ -1,94 +1,154 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-import init from './init';
+import init from './init'
 
-import './style.css';
+import './style.css'
 
-import GUI from 'lil-gui';
+import GUI from 'lil-gui'
 
 import Stats from 'stats.js'
 
-const {sizes, scene, camera, canvas, controls, renderer} = init();
+import { PLANETS } from './global.js'
 
-camera.position.z = 30;
+const { sizes, scene, camera, canvas, controls, renderer, sunLight } = init()
+camera.position.set(50, 70, 200)
 
-const textureLoader = new THREE.TextureLoader();
-
-const planetList = [
-	{name: 'Earth', position: 35, material: 'textures/earth.jpg', size: 3},
-	{name: 'Jupiter', position: 55, material: 'textures/jupiter.jpg', size: 7},
-	{name: 'Mars', position: 45, material: 'textures/mars.jpg', size: 1.5},
-	{name: 'Mercury', position: 15, material: 'textures/mercury.jpg', size: 1},
-	{name: 'Moon', position: 105, material: 'textures/moon.jpg', size: 0.4},
-	{name: 'Neptune', position: 112, material: 'textures/neptune.jpg', size: 4},
-	{name: 'Saturn', position: 75, material: 'textures/saturn.jpg', size: 6},
-	{name: 'SaturnRing', position: 125, material: 'textures/saturn.jpg', size: 6},
-	{name: 'Sun', position: 0, material: 'textures/sun.jpg', size: 10},
-	{name: 'Uranus', position: 95, material: 'textures/uranus.jpg', size: 5},
-	{name: 'Venus', position: 25, material: 'textures/venus.jpg', size: 2},
-];
-
-const allPlanets = new THREE.Group();
-
-const gui = new GUI();
-
-planetList.forEach(planet => {
-	const geometry = new THREE.SphereGeometry(planet.size, 256, 256);
-	const material = new THREE.MeshBasicMaterial({
-		map: textureLoader.load(planet.material),
-	});
-	
-	const mesh = new THREE.Mesh(geometry, material);
-	mesh.position.x = planet.position;
-	mesh.name = planet.name;
-	allPlanets.add(mesh);
-});;
-
-scene.add(allPlanets);
-
-gui.add(camera.position, 'y').min(0).max(150).name("Camera distance Y:");
-gui.add(camera.position, 'x').min(-150).max(150).name("Camera distance X:");
-gui.add(camera.position, 'z').min(30).max(150).name("Camera distance Z:");
-
-window.addEventListener('dblclick', () => {
-	if(!document.fullscreenElement){
-		canvas.requestFullscreen()
-	} else{
-		document.exitFullscreen()
-	}
-});
-
-window.addEventListener('resize', () => {
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
-
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio));
-	renderer.render(scene, camera);
+const gui = new GUI({
+	closeFolders: true,
 })
 
-const stats = new Stats();
+const orbits = new THREE.Object3D()
+const textureLoader = new THREE.TextureLoader()
+
+const sunGlowGeometry = new THREE.SphereGeometry(5, 64, 64)
+const sunGlowMaterial = new THREE.MeshBasicMaterial({
+	color: 0xffff00,
+	transparent: true,
+	opacity: 1,
+})
+
+const sunGlow = new THREE.Mesh(sunGlowGeometry, sunGlowMaterial)
+scene.add(sunGlow)
+
+PLANETS.forEach(planet => {
+	const geometry = new THREE.SphereGeometry(planet.size, 256, 256)
+	let material
+
+	if (planet.name === 'Sun') {
+		material = new THREE.MeshBasicMaterial({
+			map: textureLoader.load(planet.material),
+		})
+	} else {
+		material = new THREE.MeshPhongMaterial({
+			map: textureLoader.load(planet.material),
+		})
+	}
+
+	const mesh = new THREE.Mesh(geometry, material)
+	mesh.position.x = planet.position
+	mesh.name = planet.name
+	mesh.distance = planet.position
+	mesh.orbitTime = planet.orbitTime
+	mesh.sunAxleTime = planet.sunAxleTime
+	
+
+	if (planet.name === 'Saturn') {
+		const ringGeometry = new THREE.RingGeometry(
+			planet.size * 1.25,
+			planet.size * 2,
+			64
+		)
+		const ringMaterial = new THREE.MeshBasicMaterial({
+			color: 0xFFCC66,
+			side: THREE.DoubleSide,
+			transparent: true,
+			opacity: 0.4,
+		})
+		const ring = new THREE.Mesh(ringGeometry, ringMaterial)
+		ring.rotation.x = Math.PI / 2
+		mesh.add(ring)
+	}
+
+	orbits.add(mesh)
+})
+scene.add(orbits)
+sunLight.target = orbits.children[0];
+
+
+const planetSpeeds = {};
+const speedTime = gui.addFolder('Planets Speed');
+PLANETS.forEach(planet => {
+    if (planet.name !== 'Sun') {
+        planetSpeeds[planet.name] = planet.sunAxleTime; // Изначальная скорость
+        speedTime.add(planetSpeeds, planet.name).min(0.1).max(2).step(0.1).name(`${planet.name} Speed`);
+				planet.castShadow = true;
+				planet.receiveShadow = true;
+    }
+});
+const cameraPos = gui.addFolder('Camera Position');
+cameraPos.add(camera.position, 'x').min(50).max(150).step(10).name('Camera PositionX:');
+cameraPos.add(camera.position, 'y').min(70).max(150).step(10).name('Camera PositionY:');
+cameraPos.add(camera.position, 'z').min(200).max(400).step(10).name('Camera PositionZ:');
+
+window.addEventListener('dblclick', () => {
+	if (!document.fullscreenElement) {
+		canvas.requestFullscreen()
+	} else {
+		document.exitFullscreen()
+	}
+})
+
+window.addEventListener('resize', () => {
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
+
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.render(scene, camera)
+})
+
+const stats = new Stats()
 stats.showPanel(0)
 document.body.appendChild(stats.dom)
 
+let angle = 0
 
-const clock = new THREE.Clock();
-allPlanets.children.forEach( child => {
-	console.log(child)
-});
+const clock = new THREE.Clock()
+
+orbits.children.forEach(child => {
+	const orbitGeometry = new THREE.RingGeometry(
+		child.distance - 0.1,
+		child.distance + 0.1,
+		512
+	)
+	const orbitMaterial = new THREE.MeshBasicMaterial({
+		color: 0xffffff,
+		side: THREE.DoubleSide,
+	})
+	const orbit = new THREE.Mesh(orbitGeometry, orbitMaterial)
+	orbit.rotation.x = Math.PI / 2
+	scene.add(orbit)
+})
+
 const animate = () => {
-	stats.begin();
-	const delta = clock.getDelta();
-	controls.update();
+	stats.begin()
+	const delta = clock.getDelta()
+	controls.update()
+	angle += 0.01
 
-	allPlanets.children.forEach(child => {
-		child.rotation.y += Math.sin(delta) * 0.25;
+	orbits.children.forEach(child => {
+		child.rotation.y += Math.cos(delta) * child.orbitTime
+		if (child.name !== 'Sun') {
+			child.position.set(Math.cos(angle * planetSpeeds[child.name]) * child.distance, 0, Math.sin(angle * planetSpeeds[child.name]) * child.distance)
+		} else if (child.name === 'Saturn') {
+			child.children[0].rotation.z += 0.01
+		}
 	})
 
-	renderer.render(scene, camera);
-	
+	renderer.render(scene, camera)
 
-	requestAnimationFrame(animate);
-	stats.end();
-};
-animate();
+	requestAnimationFrame(animate)
+	stats.end()
+}
+animate()
